c489c2b05ded22b5e122dcce2d47d0cd79857072
Adam Vivent Kapuściński <a.kapuscinski@thevivent.com>
2021-02-14 15:31:10 +0100

    ajax requests ok, server side

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const urlParser = require("url");
 const port = 80;
 
 const app = express();
 app.set('view engine', 'hbs')
-app.use(express.static("src/img"))
+app.use(express.static("src"))
 
 
 app.get('/*', (req, res) => {
-    res.render('index');
+    body = "test";
+    url = urlParser.parse(req.url, true);
+
+
+    console.log(url.query.ajax)
+
+    if (url.query.ajax) {
+        res.send("asdsa")
+    }
+    else{
+
+        res.render('index', {
+            body: body
+        });
+    }
 })
 
 app.listen(port);
