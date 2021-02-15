b340e9a10b91eaecc6cc5c516fc589bbdf1748d3
Adam Vivent Kapuściński <a.kapuscinski@thevivent.com>
2021-02-15 18:44:54 +0100

    fixed ajax (accidently removed 'url' module)

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
+// imports
 const express = require("express");
+const urlParser = require("url")
 const handlebars = require("express-handlebars");
+
+// config
 const port = 80; // server port (no shit)
+const defaultLayoutName = "defaultLayout"
 
 // load app
 const app = express();
 app.set('view engine', 'hbs')
 app.engine('hbs', handlebars({
     extname: 'hbs',                             // set hbs extension
-    defaultLayout: 'defaultLayout',             // set default layout (in views/layouts)
+    defaultLayout: defaultLayoutName,             // set default layout (in views/layouts)
     layoutsDir: __dirname + '/views/layouts',   // set layouts dir
     partialsDir: __dirname + '/views/partials/' // set partials dir
 }))
@@ -16,26 +21,26 @@ app.use(express.static("public")) // make website use sources from public dir
 
 // handling pages. Needs to be moved to separate file & find better solution for handling AJAX
 app.get('/', (req, res) => {
+    url = urlParser.parse(req.url, true);
 
-    if (url.query.ajax) {
-        res.render("home",
-            {layout: false})
-    }
-    else{
-        res.render('home');
-    }
+    res.render("home",
+        {layout: (!url.query.ajax?defaultLayoutName:false)})
 
 })
 
 app.get('/CV', (req, res) => {
+    url = urlParser.parse(req.url, true);
 
-    if (url.query.ajax) {
-        res.render("CV",
-            {layout: false})
-    }
-    else{
-        res.render('CV');
-    }
+    res.render("CV",
+        {layout: (!url.query.ajax?defaultLayoutName:false)})
+
+})
+
+app.get('/projekty', (req, res) => {
+    url = urlParser.parse(req.url, true);
+
+    res.render("projekty",
+        {layout: (!url.query.ajax?defaultLayoutName:false)})
 
 })
 
